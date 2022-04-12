a4df66161ae0b38000dc98d74a23f0166cd7be8b
Vitalii Bezhanovych <bezhav@gmail.com>
2022-04-12 10:17:15 +0300

    Minor Update

--- ELB+ASG/main.tf
+++ ELB+ASG/main.tf
@@ -19,9 +19,10 @@ resource "aws_autoscaling_group" "cv" {
 
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
-
-  min_size = 3
-  max_size = 4
+  
+  desired_capacity          = 2
+  min_size = 2
+  max_size = 3
 
   tag {
     key                 = "Name"
